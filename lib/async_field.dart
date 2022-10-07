69e867bc03359c3c148a9e27cb1607562c69fe64
Mozer Dmitriy <newgener@tuta.io>
2022-10-07 22:39:30 +0700

    update async_field.dart

--- lib/async_field.dart
+++ lib/async_field.dart
@@ -1,2 +1,1 @@
 export 'package:async_field/src/async_field.dart';
-export 'package:async_field/src/error.dart';
--- lib/src/async_field.dart
+++ lib/src/async_field.dart
@@ -1,110 +1,42 @@
 import 'package:equatable/equatable.dart';
 import 'package:flutter/widgets.dart';
 
-import 'error.dart';
-import 'safe_check.dart';
-
-class AsyncField<T> extends Equatable {
+class AsyncField<T, E> extends Equatable {
   const AsyncField._({
     this.data,
     this.error,
-    this.originalError,
-    this.stackTrace,
     this.isLoading = false,
   });
 
   const AsyncField.data(T this.data)
       : error = null,
-        stackTrace = null,
-        originalError = null,
         isLoading = false;
 
   const AsyncField.error(
-    Exception this.error, {
-    this.originalError,
-    this.stackTrace,
+    E this.error, {
     this.data,
   }) : isLoading = false;
 
   const AsyncField.loading({this.data})
       : isLoading = true,
-        error = null,
-        stackTrace = null,
-        originalError = null;
+        error = null;
 
   final T? data;
-  final Exception? error;
-  final Exception? originalError;
-  final StackTrace? stackTrace;
+  final E? error;
   final bool isLoading;
 
   bool get hasData => data != null;
 
   bool get hasError => error != null;
 
-  static Future<AsyncField<T>> action<T>({
-    required Future<T?> Function() action,
-    Future<bool> Function()? check,
-  }) async {
-    final needToCheck = check != null;
-    var passed = needToCheck ? false : true;
-    Exception? originalError;
-    StackTrace? stackTrace;
-
-    if (needToCheck) {
-      final checkResult = await safeCheck(check);
-      if (checkResult.hasData) {
-        passed = checkResult.data!;
-      } else {
-        originalError = checkResult.originalError;
-        stackTrace = checkResult.stackTrace;
-      }
-    }
-
-    if (passed) {
-      T? data;
-      Exception? error;
-      try {
-        data = await action();
-      } on Exception catch (e, s) {
-        error = e;
-        originalError = error;
-        stackTrace = s;
-      } finally {
-        if (data == null && error == null) {
-          error = const DataIsNullException();
-        }
-      }
-      if (error != null) {
-        return AsyncField.error(
-          error,
-          originalError: originalError,
-          stackTrace: stackTrace,
-        );
-      } else {
-        return AsyncField.data(data as T);
-      }
-    } else {
-      return AsyncField.error(
-        const CheckNotPassed(),
-        originalError: originalError,
-        stackTrace: stackTrace,
-      );
-    }
-  }
-
   Widget when({
     required Widget Function() onLoading,
     required Widget Function(T data) onData,
-    required Widget Function(Exception error,
-            {Exception? originalError, StackTrace? stackTrace})
-        onError,
+    required Widget Function(E error) onError,
   }) {
     if (hasError) {
       return onError(
-        error!,
-        originalError: originalError,
-        stackTrace: stackTrace,
+        error as E,
       );
     }
     if (hasData) {
@@ -119,16 +51,12 @@ class AsyncField<T> extends Equatable {
   Widget maybeWhen({
     Widget Function()? onLoading,
     Widget Function(T data)? onData,
-    Widget Function(Exception error,
-            {Exception? originalError, StackTrace? stackTrace})?
-        onError,
+    Widget Function(E error)? onError,
     required Widget Function() orElse,
   }) {
     if (hasError && onError != null) {
       return onError(
-        error!,
-        originalError: originalError,
-        stackTrace: stackTrace,
+        error as E,
       );
     }
     if (hasData && onData != null) {
@@ -140,23 +68,18 @@ class AsyncField<T> extends Equatable {
     return orElse();
   }
 
-  AsyncField<T> copyWith({
+  AsyncField<T, E> copyWith({
     T? data,
-    Exception? error,
-    Exception? originalError,
-    StackTrace? stackTrace,
+    E? error,
     bool? isLoading,
   }) {
-    return AsyncField<T>._(
+    return AsyncField<T, E>._(
       data: data ?? this.data,
       error: error ?? this.error,
-      originalError: originalError ?? this.originalError,
-      stackTrace: stackTrace ?? this.stackTrace,
       isLoading: isLoading ?? this.isLoading,
     );
   }
 
   @override
-  List<Object?> get props =>
-      [data, error, stackTrace, originalError, isLoading];
+  List<Object?> get props => [data, error, isLoading];
 }
